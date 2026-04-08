a835f94071581921ba0d1d3c3b12ae12d6bb6a45
UnknowEntity <theplayer357@gmail.com>
2026-04-08 21:00:03 +0700

    feat: enhance AuthzGuard with logging for access denial

--- src/modules/authz/authz.guard.ts
+++ src/modules/authz/authz.guard.ts
@@ -1,17 +1,31 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthzService } from './authz.service';
 import { AuthRequest } from 'src/interfaces/auth.interface';
+import { Logger } from 'winston';
+import { MasterLogger } from 'src/logger/logger';
 
 @Injectable()
 export class AuthzGuard implements CanActivate {
-  constructor(private readonly authzService: AuthzService) {}
-  canActivate(context: ExecutionContext): Promise<boolean> {
+  authzLogger: Logger;
+  constructor(private readonly authzService: AuthzService) {
+    this.authzLogger = MasterLogger.child({ label: 'AuthzGuard' });
+  }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const ctx = context.switchToHttp();
     const request = ctx.getRequest<AuthRequest>();
 
     const object = request.url;
     const action = request.method;
     const subject = request.user.id.toString();
-    return this.authzService.enforce(subject, object, action);
+    const isValid = await this.authzService.enforce(subject, object, action);
+
+    if (!isValid) {
+      this.authzLogger.warn(
+        `Access denied for user ${subject} on ${object} with action ${action}`,
+        { path: request.path, method: request.method, userId: subject },
+      );
+    }
+
+    return isValid;
   }
 }

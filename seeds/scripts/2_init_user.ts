5021764f711821512539e8051e30f1cdfbd131f5
UnknowEntity <theplayer357@gmail.com>
2026-04-10 16:08:37 +0700

    refactor: remove Casbin enforcer setup from user initialization script

--- seeds/scripts/2_init_user.ts
+++ seeds/scripts/2_init_user.ts
@@ -2,19 +2,8 @@ import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { users } from 'src/database/schema';
 import { Scrypt } from 'src/utils/crypto.util';
 import { SeedConfigInterface } from '../config';
-import { newEnforcer } from 'casbin';
-import { getFilePath } from 'src/utils/app.util';
-import DrizzleAdapter, { casbinRulePostgres } from 'drizzle-adapter';
-import { CONFIG_FILENAME } from 'src/constants/auth.constant';
 
 export async function execute(db: NodePgDatabase, config: SeedConfigInterface) {
-  const a = await DrizzleAdapter.newAdapter({
-    db: db,
-    table: casbinRulePostgres,
-  });
-
-  const enforcer = await newEnforcer(getFilePath(CONFIG_FILENAME), a);
-
   if (!config.seeds_value.admin_password) {
     throw new Error('ADMIN_PASSWORD is not set in the seed configuration');
   }
@@ -27,10 +16,5 @@ export async function execute(db: NodePgDatabase, config: SeedConfigInterface) {
 
   adminUser.password = await Scrypt.hash(adminUser.password.trim());
 
-  const [admin] = await db
-    .insert(users)
-    .values(adminUser)
-    .returning({ id: users.id });
-
-  await enforcer.addGroupingPolicy(admin.id.toString(), 'admin');
+  await db.insert(users).values(adminUser);
 }

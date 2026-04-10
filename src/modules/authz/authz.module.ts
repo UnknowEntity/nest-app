5327b82d6d7379269819c294216698513fce7e3e
UnknowEntity <theplayer357@gmail.com>
2026-04-10 16:04:49 +0700

    feat: make AuthzModule global to allow shared access across the application

--- src/modules/authz/authz.module.ts
+++ src/modules/authz/authz.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthzService } from './authz.service';
 import { DatabaseModule } from 'src/database/database.module';
 
+@Global()
 @Module({
   imports: [DatabaseModule],
   providers: [AuthzService],
+  exports: [AuthzService],
 })
 export class AuthzModule {}

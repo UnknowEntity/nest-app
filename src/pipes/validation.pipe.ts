106c327c43658c1dc386979c4ab5e1159a049f3c
UnknowEntity <theplayer357@gmail.com>
2026-04-15 17:51:03 +0700

    feat: enhance ZodValidationPipe to support parameter type validation

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,12 +1,19 @@
-import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, ArgumentMetadata, Paramtype } from '@nestjs/common';
 import * as zod from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: zod.ZodObject) {}
+  constructor(
+    private schema: zod.ZodObject,
+    private type: Paramtype = 'body',
+  ) {}
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  transform(value: unknown, _: ArgumentMetadata) {
-    const parsedValue = this.schema.parse(value);
-    return parsedValue;
+  transform(value: unknown, { type }: ArgumentMetadata) {
+    if (this.type == type) {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    }
+
+    return value;
   }
 }
